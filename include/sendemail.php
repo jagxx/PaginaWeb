2bd83d1fd55c2c30c4fa931f8abf6f72306e6fa2
LanonD <vanl97@hotmail.com>
2019-06-18 16:14:55 -0500

    forms completos

--- include/sendemail.php
+++ include/sendemail.php
@@ -5,24 +5,24 @@ require_once('phpmailer/class.phpmailer.php');
 $mail = new PHPMailer();
 
 if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
-    if( $_POST['template-contactform-name'] != '' AND $_POST['template-contactform-email'] != '' AND $_POST['template-contactform-message'] != '' ) {
+    if( $_POST['template-contactform-name'] != '' AND $_POST['template-contactform-email'] != '' ) {
 
         $name = $_POST['template-contactform-name'];
         $email = $_POST['template-contactform-email'];
         $phone = $_POST['template-contactform-phone'];
+        $phone2 = $_POST['template-contactform-phone2'];
+        $phone3 = $_POST['template-contactform-phone3'];
+        $phone4 = $_POST['template-contactform-phone4'];
+        $phone5 = $_POST['template-contactform-phone5'];
+        $phone6 = $_POST['template-contactform-phone6'];
+        $phone7 = $_POST['template-contactform-phone7'];
+        $phone8 = $_POST['template-contactform-phone8'];
+        $phone9 = $_POST['template-contactform-phone9'];
+        $phone10 = $_POST['template-contactform-phone10'];
+        $phone11 = $_POST['template-contactform-phone11'];
         $subject = $_POST['template-contactform-name'];
         $message = $_POST['template-contactform-message'];
-        $service = $_POST['answer1'];
-        $r1 = $_POST['answer1'];
-        $r2 = $_POST['answer2'];
-        $r3 = $_POST['answer3'];
-        $r4 = $_POST['answer4'];
-        $r5 = $_POST['answer5'];
-        $r6 = $_POST['answer6'];
-        $r7 = $_POST['answer7'];
-        $r8 = $_POST['answer8'];
-        $r9 = $_POST['answer9'];
-        $r10 = $_POST['answer10'];
+        $message2 = $_POST['template-contactform-message2'];
 
         $subject = isset($subject) ? $subject : 'New Message From Contact Form';
 
@@ -41,39 +41,44 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
             $name = isset($name) ? "Nombre: $name<br><br>" : '';
             $email = isset($email) ? "Email: $email<br><br>" : '';
             $phone = isset($phone) ? "Telefono: $phone<br><br>" : '';
-            $message = isset($message) ? "Mensaje: $message<br><br>" : '';
-            $service = isset($service) ? "Respuesta1: $service<br><br>" : '';
-            $r1 = isset($r1) ? "Tipo de web: $r1<br><br>" : '';
-            $r2 = isset($r2) ? "Diseño: $r2<br><br>" : '';
-            $r3 = isset($r3) ? "Tamaño: $r3<br><br>" : '';
-            $r4 = isset($r4) ? "Pagos: $r4<br><br>" : '';
-            $r5 = isset($r5) ? "Integraciones: $r5<br><br>" : '';
-            $r6 = isset($r6) ? "Login: $r6<br><br>" : '';
-            $r7 = isset($r7) ? "Multidioma: $r7<br><br>" : '';
-            $r8 = isset($r8) ? "Buscador: $r8<br><br>" : '';
-            $r9 = isset($r9) ? "SEO: $r9<br><br>" : '';
-            $r10 = isset($r10) ? "Status: $r10<br><br>" : '';
+            $message = isset($message) ? "Respuestas: $message<br><br>" : '';
+            $message2 = isset($message2) ? "Mensaje: $message2<br><br>" : '';
+
+            $phone2 = explode("- ", $message);
+            $phone3 = "$phone2[0] <br>";
+            $phone4 = "$phone2[1] <br>";
+            $phone5 = "$phone2[2] <br>";
+            $phone6 = "$phone2[3] <br>";
+            $phone7 = "$phone2[4] <br>";
+            $phone8 = "$phone2[5] <br>";
+            $phone9 = "$phone2[6] <br>";
+            $phone10 = "$phone2[7] <br>";
+            $phone11 = "$phone2[8] <br>";
+            $phone12 = "$phone2[9] <br>";
+            $phone13 = "$phone2[10] <br>";
+            $phone14 = "$phone2[11] <br>";
 
             $referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This Form was submitted from: ' . $_SERVER['HTTP_REFERER'] : '';
 
-            $body = "$name $email $phone $message $service $r1 $r2 $r3 $r4 $r5 $r6 $r7 $r8 $r9 $r10 $referrer";
+            $body = "$name $email $phone $phone3 $phone4 $phone5 $phone6 $phone7 $phone8 $phone9 $phone10 $phone11 $phone12 $phone13 $phone14 $message2 $service $referrer";
 
             $mail->MsgHTML( $body );
+            $mail->CharSet = "UTF­8";
             $sendEmail = $mail->Send();
 
             if( $sendEmail == true ):
-                echo 'We have <strong>successfully</strong> received your Message and will get Back to you as soon as possible.';
+                echo 'Recibimos <strong>exitosamente</strong> su mensaje, lo contactaremos lo antes posible.';
             else:
-                echo 'Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '';
+                echo 'Ocurrió un error, favor de intentarlo más tarde.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '';
             endif;
         } else {
-            echo 'Bot <strong>Detected</strong>.! Clean yourself Botster.!';
+            echo '<strong>Bot detectado</strong>.!';
         }
     } else {
-        echo 'Please <strong>Fill up</strong> all the Fields and Try Again.';
+        echo 'Por favor rellena todas las columnas e intenta de nuevo.';
     }
 } else {
-    echo 'An <strong>unexpected error</strong> occured. Please Try Again later.';
+    echo 'Ocurrió un error inesperado, por favor intenta más tarde.';
 }
 
 ?>
